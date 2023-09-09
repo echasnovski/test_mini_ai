9d87818e7ddf58eb3a221b04535c0c2a47cb205b
abeldekat <abel@nomail.com>
2023-09-09 19:44:03 +0200

    testing for mini.ai, issue 474

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,15 +8,6 @@ local opts = {
     "luadoc",
     "luap",
   },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      scope_incremental = false,
-      node_decremental = "<bs>",
-    },
-  },
 }
 
 require("nvim-treesitter.configs").setup(opts)
